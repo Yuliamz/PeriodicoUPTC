59a34a544c483ebbdad80c15649bf3bcb4f7b0a7
Julian David <caprijuda@gmail.com>
2017-12-10 17:43:09 -0500

    added actions until save,delete,cancel,edit and update

--- src/main/webapp/js/ArticuloController.js
+++ src/main/webapp/js/ArticuloController.js
@@ -3,8 +3,38 @@
 module.controller('ArticuloCtrl', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
     //listar
     $scope.lista = [];
+    $scope.listaPalabraClave = [];
+    $scope.tags = [];
     $scope.datosFormulario = {};
     $scope.panelEditar = false;
+
+    $scope.visto = function() {
+        //            $scope.datosFormulario.texto = $('#texto').froalaEditor('html.get');
+        //            console.log($scope.datosFormulario.texto);
+        //            $scope.toBase64();
+        console.log(JSON.stringify($scope.tags));
+    };
+
+    $("#imagen").on('change', function() {
+        var selectedFile = this.files[0];
+        if (selectedFile) {
+            $scope.toBase64(selectedFile);
+        }
+    });
+
+    $scope.toBase64 = function(file) {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function() {
+            $scope.datosFormulario.imagen = reader.result;
+            console.log($scope.datosFormulario.imagen);
+            $('#displayImage').attr('src', $scope.datosFormulario.imagen);
+        };
+        reader.onerror = function(error) {
+            console.log('Error: ', error);
+        };
+    };
+
     $scope.listar = function() {
         $http.get('./webresources/Articulo', {})
             .success(function(data, status, headers, config) {
@@ -13,6 +43,16 @@ module.controller('ArticuloCtrl', ['$scope', '$filter', '$http', function($scope
                 alert('Error al consultar la informaci\xf3n, por favor intente m\xe1s tarde');
             });
     };
+
+    $scope.listarPalabraClave = function() {
+        $http.get('./webresources/Tag', {})
+            .success(function(data, status, headers, config) {
+                $scope.listaPalabraClave = data;
+            }).error(function(data, status, headers, config) {
+                alert('Error al consultar la informaci\xf3n de palabraClave, por favor intente m\xe1s tarde');
+            });
+    };
+    $scope.listarPalabraClave();
 
     $scope.listarSeccion = function() {
         $http.get('./webresources/Seccion', {})
@@ -30,6 +70,10 @@ module.controller('ArticuloCtrl', ['$scope', '$filter', '$http', function($scope
     $scope.nuevo = function() {
         $scope.panelEditar = true;
         $scope.datosFormulario = {};
+        $('#texto').froalaEditor('html.set', "<p> Contenido del Artículo </p>");
+        $('#displayImage').removeAttr("src");
+        $('#tags').value = "";
+        $('#tags').fastselect();
     };
 
     $scope.guardar = function() {
@@ -38,8 +82,11 @@ module.controller('ArticuloCtrl', ['$scope', '$filter', '$http', function($scope
 
         if (error)
             return;
+        $scope.datosFormulario.texto = $('#texto').froalaEditor('html.get');
+        $scope.datosFormulario.tags = $scope.tags.join(", ");
         $http.post('./webresources/Articulo', JSON.stringify($scope.datosFormulario), {}).success(function(data, status, headers, config) {
             alert("Los datos han sido guardados con Exito");
+            console.log(JSON.stringify($scope.datosFormulario));
             $scope.panelEditar = false;
             $scope.listar();
         }).error(function(data, status, headers, config) {
@@ -55,6 +102,10 @@ module.controller('ArticuloCtrl', ['$scope', '$filter', '$http', function($scope
     $scope.editar = function(data) {
         $scope.panelEditar = true;
         $scope.datosFormulario = data;
+        $('#texto').froalaEditor('html.set', $scope.datosFormulario.texto);
+        $('#displayImage').attr('src', $scope.datosFormulario.imagen);
+        $('#imagen').value = "";
+        $('#tags').fastselect().data($scope.datosFormulario.palabrasClave);
     };
     //eliminar
     $scope.eliminar = function(data) {
